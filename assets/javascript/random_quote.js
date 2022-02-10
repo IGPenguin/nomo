5af12bdea1a37593245e0bae5f909721b4715b89
Adam Svoboda <eidamsvoboda@gmail.com>
2022-02-10 23:29:00 +0100

    Remove .csv load function content for testing

--- assets/javascript/random_quote.js
+++ assets/javascript/random_quote.js
@@ -8,27 +8,10 @@ $(document).ready(function() {
 });
 
 function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
-
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
-        }
-    }
     console.log(generateRandomInteger(10))
     alert(generateRandomInteger(10))
-    alert(lines[0].join());
 }
 
-// Generate a number between 0 and max, including max
 function generateRandomInteger(max) {
     return Math.floor(Math.random() * max) + 1;
 }
