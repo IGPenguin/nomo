2788393b23effb91054566aa63b59cb2c06a95cd
Adam Svoboda <eidamsvoboda@gmail.com>
2022-02-10 23:37:31 +0100

    Attempt to log .csv contents to console

--- assets/javascript/random_quote.js
+++ assets/javascript/random_quote.js
@@ -8,8 +8,23 @@ $(document).ready(function() {
 });
 
 function processData(allText) {
+  var allTextLines = allText.split(/\r\n|\n/);
+  var headers = allTextLines[0].split(',');
+  var lines = [];
+
+  for (var i=1; i<allTextLines.length; i++) {
+      var data = allTextLines[i].split(',');
+      if (data.length == headers.length) {
+
+          var tarr = [];
+          for (var j=0; j<headers.length; j++) {
+              tarr.push(headers[j]+":"+data[j]);
+          }
+        lines.push(tarr);
+    }
+}
+    console.log(lines.join())
     console.log(generateRandomInteger(10))
-    alert(generateRandomInteger(10))
 }
 
 function generateRandomInteger(max) {
